cbe88f6214c4d680b3d61aa897246bba61865ba6
Aishwarya Pant <aishpant@gmail.com>
2018-03-05 12:44:34 +0530

    display README on pypi
    Provide a long description in setuptools to make display work.
    
    Add licensing information and python requirement (>3.0).
    
    Signed-off-by: Aishwarya Pant <aishpant@gmail.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 setup(
     name = 'abi2doc',
     description = 'Linux Kernel sysfs attribute documentation generator',
+    long_description = readme(),
     packages = ['abi2doc'],
     include_package_data=True,
-    version = '1.2',
+    version = '1.3',
     author = 'Aishwarya Pant',
     author_email = 'aishpant@gmail.com',
+    license = 'GPLv2',
+    python_requires = '>=3',
     url = 'https://github.com/aishpant/attribute-documentation',
-    download_url = 'https://github.com/aishpant/attribute-documentation/archive/1.2.tar.gz',
+    download_url = 'https://github.com/aishpant/attribute-documentation/archive/1.3.tar.gz',
     keywords = 'documentation sysfs linux',
     entry_points={
         'console_scripts': [
